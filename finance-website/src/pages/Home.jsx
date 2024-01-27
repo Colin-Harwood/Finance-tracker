87bf36a00b1d5cdb6b88fae6285fe82456a8afe8
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-27 15:54:16 +0200

    Set up tailwind

--- finance-website/src/pages/Home.jsx
+++ finance-website/src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import './Home.css';
 
+
 const Home = () => {
   return (
     <>
       <div className="home-container">
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
-        <h1>Test</h1>
+      <h1 className="text-8xl font-bold underline">
+      Hello world!
+    </h1>
+    <p className="text-3xl underline">
+      This is a simple React app that uses the <a href="https://polygon.io">Polygon.io</a> API to display stock data.
+    </p>
       </div>
     </>
   );
