835a84eccc690f99a9e67ea90f235a0f1d2e707f
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-27 17:15:20 +0200

    Set up basic layout for hero of home page

--- finance-website/src/pages/Home.jsx
+++ finance-website/src/pages/Home.jsx
@@ -5,14 +5,23 @@ import './Home.css';
 const Home = () => {
   return (
     <>
-      <div className="home-container">
-      <h1 className="text-8xl font-bold underline">
-      Hello world!
-    </h1>
-    <p className="text-3xl underline">
-      This is a simple React app that uses the <a href="https://polygon.io">Polygon.io</a> API to display stock data.
-    </p>
-      </div>
+    <div className="flex flex-row justify-center content-center " id="welcome-text">
+      <div className="flex flex-col home-container h-screen justify-center">
+        <h1 className="text-9xl sm-text-3xl font-bold text-center">
+          Hello world!
+        </h1>
+        <p className="text-3xl">
+          This is a finance tracker website.
+        </p>
+        <button className="text-left mt-10 " id='SignUpButton'>
+          Sign Up
+        </button>
+        </div>
+    </div>
+    <div className="h-96" id="services">
+      <p>Test</p>
+    </div>
+      
     </>
   );
 };
