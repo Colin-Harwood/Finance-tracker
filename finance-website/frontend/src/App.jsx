651a33b7eb28acec3b6ca6bd3ee467b909cc3847
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-28 14:08:40 +0200

    Moved all frontend into dedicated frontend folder

--- finance-website/frontend/src/App.jsx
+++ finance-website/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
---
+++ finance-website/frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar.jsx';
+
+const Dashboard = () => {
+  return (
+    <>
+    <Navbar/>
+    <div>Dashboard</div>
+
+    </>
+  )
+}
+
+export default Dashboard
