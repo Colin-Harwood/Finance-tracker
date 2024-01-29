0c8d83324df458d85a60e1270541a53fff3f4e7c
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-29 17:45:12 +0200

    Set out the grid layout for the dashboard

--- finance-website/frontend/src/components/Sidebar.jsx
+++ finance-website/frontend/src/components/Sidebar.jsx
@@ -33,15 +33,23 @@ const Sidebar = () => {
           <img src="/icons8-menu-30.png" alt="Hamburger Menu Icon" width="30px"/>
         </button>
         <ul>
-          <li>
-            <Link to="/page1">Page 1</Link>
+          <Link to="/page1">
+          <li className="mb-2 text-2xl">
+          <h3>Page 1</h3>
           </li>
-          <li>
-            <Link to="/page2">Page 2</Link>
+          </Link>
+          <hr />
+          <Link to="/page2">
+          <li className="mb-2 text-2xl mt-2">
+            <h3>Page 2</h3>
           </li>
-          <li>
-            <Link to="/page3">Page 3</Link>
+          </Link>
+          <hr />
+          <Link to="/page1">
+          <li className="mt-2 text-2xl">
+            <h3>Page 3</h3>
           </li>
+          </Link>
           {/* Add more links as needed */}
         </ul>
       </div>
--- finance-website/frontend/src/pages/Dashboard.jsx
+++ finance-website/frontend/src/pages/Dashboard.jsx
@@ -9,7 +9,33 @@ const Dashboard = () => {
     <Navbar/>
     < Sidebar />
     
-    <div>Dashboard</div>
+    <div className='p-10 ml-5'>
+      <div className="flex justify-center lg:text-7xl text-5xl">
+        <h1><u>Names Dashboard</u></h1>
+      </div>
+      <div className="grid grid-cols-11 gap-5 mt-16">
+        <div className="lg:col-span-4 md:col-span-7 col-span-11 column">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+        <div className="lg:col-span-2 md:col-span-4 col-span-11 column">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+        <div className="lg:col-span-5 col-span-11 column h-32">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+      </div>
+      <div className="grid grid-cols-11 gap-5 mt-5">
+        <div className="lg:col-span-3 md:col-span-6 col-span-11 column">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+        <div className="lg:col-span-3 md:col-span-5 col-span-11 column">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+        <div className="lg:col-span-5 col-span-11 column IandE">
+          <p className="text-2xl text-center"><b>Head Name</b></p>
+        </div>
+      </div>
+    </div>
 
     </>
   )
