631c621c0e239010bd6b77c7bc0e9a6982a1331d
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-29 22:48:53 +0200

    Added section for Income page

--- finance-website/frontend/src/pages/Income.jsx
+++ finance-website/frontend/src/pages/Income.jsx
@@ -8,7 +8,14 @@ const Income = () => {
         <>
         <Navbar/>
         < Sidebar />
-        <p>Income</p>
+        <div className="p-10">
+            <div id="income-head" className="flex justify-center lg:text-7xl text-5xl">
+                <h1 className="">Income Sources</h1>
+            </div>
+            <div id="income-all" className="mx-auto w-9/12">
+
+            </div>
+        </div>
         </>
     )
 }
