ba2fe01d3f44afec6db5d77ca0ad5652b99b1902
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-30 12:26:24 +0200

    Added headings to income sources page

--- finance-website/frontend/src/pages/Expenses.jsx
+++ finance-website/frontend/src/pages/Expenses.jsx
@@ -8,7 +8,25 @@ const Expenses = () => {
         <>
         <Navbar/>
         < Sidebar />
-        <p>Expenses</p>
+        <div className="p-10">
+            <div id="income-head" className="flex justify-center lg:text-7xl text-5xl">
+                <h1 className="">Income Sources</h1>
+            </div>
+            <div id="income-all" className="mx-auto lg:w-9/12 w-11/12">
+                <div className="grid grid-cols-3">
+                    <div className="columns-income-head">
+                        <h3 className="px-3 py-1">Category</h3>
+                    </div>
+                    <div className="columns-income-head column-middle">
+                        <h3 className="px-3 py-1">Amount</h3>
+                    </div >
+                    <div className="columns-income-head">
+                        <h3 className="px-3 py-1">Update</h3>
+                    </div>
+                </div>
+                <hr/>
+            </div>
+        </div>
         </>
     )
 }
--- finance-website/frontend/src/pages/Income.jsx
+++ finance-website/frontend/src/pages/Income.jsx
@@ -12,8 +12,19 @@ const Income = () => {
             <div id="income-head" className="flex justify-center lg:text-7xl text-5xl">
                 <h1 className="">Income Sources</h1>
             </div>
-            <div id="income-all" className="mx-auto w-9/12">
-
+            <div id="income-all" className="mx-auto lg:w-9/12 w-11/12">
+                <div className="grid grid-cols-3">
+                    <div className="columns-income-head">
+                        <h3 className="px-3 py-1">Category</h3>
+                    </div>
+                    <div className="columns-income-head column-middle">
+                        <h3 className="px-3 py-1">Amount</h3>
+                    </div >
+                    <div className="columns-income-head">
+                        <h3 className="px-3 py-1">Update</h3>
+                    </div>
+                </div>
+                <hr/>
             </div>
         </div>
         </>
