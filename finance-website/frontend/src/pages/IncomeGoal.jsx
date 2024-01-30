fcdcd97803573546a2d35d751be63ff3404df583
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-01-30 13:53:42 +0200

    Updated pages within the user portal

--- finance-website/frontend/src/pages/IncomeGoal.jsx
+++ finance-website/frontend/src/pages/IncomeGoal.jsx
@@ -9,6 +9,16 @@ const IncomeGoal = () => {
         <Navbar/>
         < Sidebar />
         <p>Income Goal</p>
+        <div className="flex justify-center mt-10">
+      <div className="flex flex-col content-center rounded-md skill-div">
+        <div className="skill px-4 flex flex-row">
+          <h3 className="pr-7">Django</h3>
+          <div className="skill-bar ">
+            <div className="skill-level" style={{width: '87%'}}></div>
+          </div>
+        </div>
+        </div>
+        </div>
         </>
     )
 }
--- finance-website/frontend/src/pages/Subscriptions.jsx
+++ finance-website/frontend/src/pages/Subscriptions.jsx
@@ -7,8 +7,26 @@ const Subscriptions = () => {
     return (
         <>
         <Navbar/>
-        <Sidebar />
-        <p>subscriptions</p>
+        < Sidebar />
+        <div className="p-10">
+            <div id="subscriptions-head" className="flex justify-center lg:text-7xl text-5xl">
+                <h1 className="">Subscriptions</h1>
+            </div>
+            <div id="subscriptions-all" className="mx-auto lg:w-9/12 w-11/12">
+                <div className="grid grid-cols-3">
+                    <div className="columns-subscriptions-head">
+                        <h3 className="px-3 py-1">Category</h3>
+                    </div>
+                    <div className="columns-subscriptions-head column-middle-subscriptions">
+                        <h3 className="px-3 py-1">Amount</h3>
+                    </div >
+                    <div className="columns-subscriptions-head">
+                        <h3 className="px-3 py-1">Update</h3>
+                    </div>
+                </div>
+                <hr/>
+            </div>
+        </div>
         </>
     )
 }
