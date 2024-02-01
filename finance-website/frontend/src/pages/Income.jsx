b356f69779ed3ba6b4c116bc606feba1c288fa6c
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-01 20:01:26 +0200

    Added the modal and form for adding income

--- finance-website/frontend/src/pages/Income.jsx
+++ finance-website/frontend/src/pages/Income.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Navbar } from '../components/Navbar.jsx';
 import './Income.css'
 import Sidebar from '../components/Sidebar.jsx';
+import Modal from 'react-modal';
+
+// Make sure to bind modal to your appElement (http://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement('#root');
 
 const Income = () => {
+    const [showForm, setShowForm] = useState(false);
+
+  const handleAddClick = () => {
+    setShowForm(true);
+  };
+
+  const closeModal = () => {
+    setShowForm(false);
+  };
+
+  const customStyles = {
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        zIndex: 1000, // Add this line
+      },
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)'
+    }
+  };
 
 
     return (
@@ -40,11 +68,30 @@ const Income = () => {
                 </div>
                 <hr/>
                 <div className="flex flex-row justify-end items-end">
-                
-                <button onClick="" id="addButton">
+                <button onClick={handleAddClick} id="addButton">
                     <img src="/icons8-plus-96.png" width="65px" alt="plus"></img>
                 </button>
                 </div>
+                <Modal
+                isOpen={showForm}
+                onRequestClose={closeModal}
+                contentLabel="Add Income Source"
+                className="Modal flex flex-col items-center justify-center backdrop-brightness-50 h-screen w-screen"
+                overlayClassName="Overlay"
+                style={customStyles}
+                >
+                
+                <form className="h-96 flex flex-col items-center justify-center">
+                    <div className="flex flex-row justify-end w-full mr-3 px-5 mb-5" style={{marginTop: '-90px'}}>
+                        <button onClick={closeModal} class="close-income-modal right-0">Close</button>
+                    </div>
+                    <label htmlFor="category">Category</label>
+                    <input type="text" id="category" name="category" placeholder="Category" required />
+                    <label htmlFor="amount">Amount</label>
+                    <input type="text" id="amount" name="amount" placeholder="Amount" required />
+                    <button type="submit" id="submit">Add</button>
+                </form>
+                </Modal>
             </div>
         </div>
         </>
