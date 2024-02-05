2968d1abedb5798fc64e5c734eb76f6b57ce20ef
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-05 22:24:54 +0200

    Added the totals for the month for each section in the dashboard

--- finance-website/frontend/src/pages/Dashboard.jsx
+++ finance-website/frontend/src/pages/Dashboard.jsx
@@ -44,6 +44,21 @@ const Dashboard = () => {
     totalIncomeThisMonth = currentMonthIncomes.reduce((total, income) => total + parseFloat(income.amount), 0);
   }
 
+  let totalExpenseThisMonth = 0;
+
+  if (info && info.expenses) {
+    const currentMonthExpenses = info.expenses.filter(expense => parseInt(expense.month) === currentMonth);
+    totalExpenseThisMonth = currentMonthExpenses.reduce((total, expense) => total + parseFloat(expense.amount), 0);
+  }
+
+  let totalSubscriptions = 0;
+
+  if (info && info.subscriptions) {
+    totalSubscriptions = info.subscriptions.reduce((total, subscription) => total + parseFloat(subscription.amount), 0);
+  }
+
+  const monthOverview = totalIncomeThisMonth - totalExpenseThisMonth;
+
   const incomeGoalValue = info ? info.incomeGoal : '';
 
   return (
@@ -58,10 +73,12 @@ const Dashboard = () => {
       <div className="grid grid-cols-11 gap-5 mt-16">
         <div className="lg:col-span-4 md:col-span-7 col-span-11 column">
           <p className="text-2xl text-center"><b>Month Overview</b></p>
+          <p className="text-2xl text-center"><b>{monthOverview}</b></p>
         </div>
         
         <div className="lg:col-span-2 md:col-span-4 col-span-11 column">
           <p className="text-2xl text-center"><b>Subscriptions</b></p>
+          <p className="text-2xl text-center"><b>{totalSubscriptions}</b></p>
           {info && info.subscriptions
             .map((income, index) => (
               <p key={index}>{`${income.source}: ${income.amount}`}</p>
@@ -80,7 +97,8 @@ const Dashboard = () => {
       <div className="grid grid-cols-11 gap-5 mt-5">
         
         <div className="lg:col-span-3 md:col-span-6 col-span-11 column">
-          <p className="text-2xl text-center"><b>Income Sources</b></p>
+          <p className="text-2xl text-center"><b>Income</b></p>
+          <p className="text-2xl text-center"><b>{totalIncomeThisMonth}</b></p>
           {info && info.incomes
               .filter(income => income.year === currentYear && income.month === currentMonthString)
               .map((income, index) => (
@@ -95,6 +113,8 @@ const Dashboard = () => {
         
         <div className="lg:col-span-3 md:col-span-5 col-span-11 column">
           <p className="text-2xl text-center"><b>Spending</b></p>
+          <p className="text-2xl text-center"><b>{totalExpenseThisMonth}</b></p>
+
           {info && info.expenses
             .filter(expense => expense.year === currentYear && expense.month === currentMonthString)
             .map((income, index) => (
