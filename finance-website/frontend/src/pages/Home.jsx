25e45d68f38192817414accadb7eadb12f6fb3ed
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-11 19:36:39 +0200

    Update Home.css and Home.jsx and the services content on the home page

--- finance-website/frontend/src/pages/Home.jsx
+++ finance-website/frontend/src/pages/Home.jsx
@@ -57,23 +57,32 @@ const Home = () => {
         </div>
     </div>
     <div className="h-96 columns-1 md:columns-1 lg:columns-3 lg:px-8" id="services">
-      <div className="flex flex-col items-center justify-around h-full">
+      <div className="flex flex-col items-center justify-around h-full serviceColumn">
         <div className="serviceIconCircle flex justify-center items-center">
           <img src="/icons8-dashboard-96.png" alt="Dashboard Icon" width="96px"/>
         </div>
-        <h2 className="text-center text-4xl font-bold">Overview/Dashboard</h2>
+        <h2 className="text-center text-4xl font-bold">Comprehensive Dashboard</h2>
+        <p className="px-10 text-center pt-5 mb-5">
+          Stay in control of your finances with our comprehensive overview dashboard, providing a clear snapshot of your financial health at a glance.
+        </p>
       </div>
-      <div className="flex flex-col items-center justify-around h-full">
-        <div className="serviceIconCircle flex justify-center items-center">
+      <div className="flex flex-col items-center justify-around h-full serviceColumn">
+        <div className="serviceIconCircle flex justify-center items-center ">
           <img src="/icons8-money-96.png" alt="Income Icon" width="96px"/>
         </div>
-        <h2 className="text-center text-4xl font-bold">Easily update</h2>
+        <h2 className="text-center text-4xl font-bold">Easy Updates</h2>
+        <p className="px-10 text-center pt-5 mb-5">
+        Effortlessly manage your income and expenses with our user-friendly interface, enabling seamless updates to reflect your financial activity in real-time.
+        </p>
       </div>
-      <div className="flex flex-col items-center justify-around h-full">
+      <div className="flex flex-col items-center justify-around h-full serviceColumn">
         <div className="serviceIconCircle flex justify-center items-center">
           <img src="/icons8-goal-96.png" alt="Goals Icon" width="96px"/>
         </div>
         <h2 className="text-center text-4xl font-bold">Easily Set goals</h2>
+        <p className="px-10 text-center pt-5 mb-5">
+        Set and track your income goals effortlessly with our intuitive platform, empowering you to define and achieve your financial milestones with ease.
+        </p>
       </div>
     </div>
     </div>
