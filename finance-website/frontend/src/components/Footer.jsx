f71983d42f324c93546f96fcbf0d3426c5eb70e0
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-13 10:55:53 +0200

    Update website favicon and footer logo

--- finance-website/frontend/src/components/Footer.jsx
+++ finance-website/frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/StockGraph.webp" className="h-8" alt="Flowbite Logo" />
+                <img src="/logo-no-background.png" className="h-8" alt="Flowbite Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
