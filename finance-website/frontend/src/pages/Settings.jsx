6596de0c4e44affa5f0ff90585af79c87e8435b6
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-15 16:10:53 +0200

    Joined the two settings forms together and updated the spacing of the individual elements fo the form

--- finance-website/frontend/src/pages/Settings.jsx
+++ finance-website/frontend/src/pages/Settings.jsx
@@ -7,21 +7,22 @@ const Settings = () => {
   return (
     <>
         <Navbar />
-        <div id="income-head" className="flex justify-center lg:text-7xl text-5xl mx-auto w-screen text-center pt-10">
+        <div className="flex justify-center lg:text-7xl text-5xl mx-auto w-screen text-center pt-10">
             <h1 className="">Settings</h1>
         </div>
         <div className="flex flex-col items-center justify-center mt-10" id="settingsPanel">
-        <form action="/change-password" method="post" className="flex flex-col items-center p-10 settingsForm">
-            <h1 className='text-4xl mb-4'>Change Password:</h1>
-            <label htmlFor="current-password">Current Password:</label><br/>
-            <input type="password" id="current-password" name="current-password"/><br/>
-            <label htmlFor="new-password">New Password:</label><br/>
-            <input type="password" id="new-password" name="new-password"/><br/>
+        <form action="/change-password" method="post" className="flex flex-col items-center p-10 settingsForm" id="passwordForm">
+            <h1 className='text-4xl mb-4'>Change Password:</h1><br/>
+            <label htmlFor="current-password" className="mb-3">Current Password:</label>
+            <input type="password" id="current-password" name="current-password"/>
+            <label htmlFor="new-password" className="mb-3">New Password:</label>
+            <input type="password" id="new-password" name="new-password"/>
             <input type="submit" value="Change Password"/>
         </form>
-        <form action="/delete-account" className="flex flex-col items-center justify-center mt-10" method="post">
-            <label htmlFor="confirm-password">Confirm Password:</label><br/>
-            <input type="password" id="confirm-password" name="confirm-password"/><br/>
+        <form action="/delete-account" className="flex flex-col items-center justify-center settingsForm mb-20" method="post" id="deleteForm">
+            <h1 className='text-4xl mb-4'>Delete account:</h1>
+            <label htmlFor="confirm-password" className="mb-3">Confirm Password:</label>
+            <input type="password" id="confirm-password" name="confirm-password"/>
             <input type="submit" value="Delete Account"/>
         </form>
         </div>
