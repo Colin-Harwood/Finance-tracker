17888a565efd115c223692f96adf568395529719
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-15 15:51:17 +0200

    Added base forms for password and account deletion in settings

--- finance-website/frontend/src/pages/Settings.jsx
+++ finance-website/frontend/src/pages/Settings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar.jsx';
 import Footer from '../components/Footer.jsx';
+import './Settings.css';
 
 const Settings = () => {
   return (
@@ -9,13 +10,19 @@ const Settings = () => {
         <div id="income-head" className="flex justify-center lg:text-7xl text-5xl mx-auto w-screen text-center pt-10">
             <h1 className="">Settings</h1>
         </div>
-        <div className="flex flex-col items-center justify-center ">
-        <form action="/change-password" method="post" className="flex flex-col items-center p-10">
+        <div className="flex flex-col items-center justify-center mt-10" id="settingsPanel">
+        <form action="/change-password" method="post" className="flex flex-col items-center p-10 settingsForm">
+            <h1 className='text-4xl mb-4'>Change Password:</h1>
             <label htmlFor="current-password">Current Password:</label><br/>
             <input type="password" id="current-password" name="current-password"/><br/>
             <label htmlFor="new-password">New Password:</label><br/>
             <input type="password" id="new-password" name="new-password"/><br/>
             <input type="submit" value="Change Password"/>
+        </form>
+        <form action="/delete-account" className="flex flex-col items-center justify-center mt-10" method="post">
+            <label htmlFor="confirm-password">Confirm Password:</label><br/>
+            <input type="password" id="confirm-password" name="confirm-password"/><br/>
+            <input type="submit" value="Delete Account"/>
         </form>
         </div>
         <Footer />
