27b526b717db9d5efa8b69d53fef708b2a36480a
Colin-Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-02-17 18:14:55 +0200

    Got the docker to work with the new server.js for the frontend

--- finance-website/frontend/server.js
+++ finance-website/frontend/server.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 5173;
 
-// Serve static files from the React frontend app
-app.use(express.static(path.resolve(path.dirname(''), 'build')))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Anything that doesn't match the above, send back index.html
+// Serve static files from the Vite build output directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch-all route to let the React router handle routing
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(path.dirname(''), 'build', 'index.html'))
-})
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
